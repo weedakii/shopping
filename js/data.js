3faf8562281176741a4b8f0854a497d1a87ed99c
weedakii <74677859+weedakii@users.noreply.github.com>
2021-10-22 17:56:07 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -43,5 +43,94 @@ let productsMom = [
         qty: 1,
         me: "n",
         love: "black"
-    }
+    },
+    {
+        id: 5,
+        title: "watch",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "watch",
+        imgUrl: "img/img6.jpg",
+        price: "200",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 6,
+        title: "bed",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "furneture",
+        imgUrl: "img/img3.jpg",
+        price: "300",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 7,
+        title: "small bed",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "other",
+        imgUrl: "img/BEBES-1.jpg",
+        price: "150",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 8,
+        title: "chair",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "devices",
+        imgUrl: "img/chair2.jpg",
+        price: "150",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 9,
+        title: "bed",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "furneture",
+        imgUrl: "img/img3.jpg",
+        price: "300",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 10,
+        title: "small bed",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "other",
+        imgUrl: "img/BEBES-1.jpg",
+        price: "150",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    {
+        id: 11,
+        title: "chair",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "devices",
+        imgUrl: "img/chair2.jpg",
+        price: "150",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
+    
+    {
+        id: 12,
+        title: "watch",
+        desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        type: "watch",
+        imgUrl: "img/img6.jpg",
+        price: "200",
+        qty: 1,
+        me: "n",
+        love: "black"
+    },
 ]
